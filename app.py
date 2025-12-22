fbee16e693e500b7e2a7868440587736693e3167
Axis-JM <jaromalo484@gmail.com>
2025-12-22 15:09:23 +0100

    add products and bodovani

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.info('Toto je informační koutek zobrazení dat:')
 
 
 
-_bodovani, _products = st.tabs(['Bodování řidičů', 'Prodeje e-shopu', 'Top filmy'])
+_bodovani, _products = st.tabs(['Bodování řidičů', 'Prodeje e-shopu'])
 
 with _bodovani:
     _1, _2 = st.columns(2)
