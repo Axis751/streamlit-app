6d4c447fffbc25c94c78ffe0b44f974a96221373
Axis-JM <jaromalo484@gmail.com>
2026-01-11 20:17:42 +0100

    add products and bodovani

--- app.py
+++ app.py
@@ -138,12 +138,9 @@ with _filmy:
                  'year': 'Rok'
              })
 
-
-    st.markdown(
-        "<p style='text-align: center; font-size: 16px; font-weight: 600;'>"
-        "Počet filmů podle roku vydání</p>",
-        unsafe_allow_html=True
-    )
+    st.write("")  # malá mezera
+    st.subheader("Počet filmů podle roku vydání")
+    st.write("")  # malá mezera
 
     st.bar_chart(df_filmy_year,
                  x='rok',
